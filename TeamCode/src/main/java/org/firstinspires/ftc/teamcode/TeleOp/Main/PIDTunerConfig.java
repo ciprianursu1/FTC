2bd78420391dab8aff4ab88d3d3d8c255381feb7
Ciprian Ursu <ursuciprian210@gmail.com>
2026-03-01 12:32:06 +0200

    updated dependencies + web pid tuner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Main/PIDTunerConfig.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.TeleOp.Main;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.configurables.annotations.Sorter;
+
+@Configurable
+public class PIDTunerConfig {
+    @Sorter(sort = 0)
+    public static double kP = 0.0;
+    @Sorter(sort = 1)
+    public static double kI = 0.0;
+    @Sorter(sort = 2)
+    public static double kD = 0.0;
+    @Sorter(sort = 3)
+    public static int targetTicks = 0;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Main/PanelsPIDTuner.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.TeleOp.Main;
+
+import com.bylazar.panels.Panels;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name = "PID Tuner (Panels)", group = "TEST")
+public class PanelsPIDTuner extends OpMode {
+    private DcMotorEx motor;
+    private PIDController pid;
+    private TelemetryManager telemetryM;
+    @Override
+    public void init() {
+        motor = hardwareMap.get(DcMotorEx.class, "spinner");
+        motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+        pid = new PIDController(
+                PIDTunerConfig.kP,
+                PIDTunerConfig.kI,
+                PIDTunerConfig.kD
+        );
+    }
+
+    @Override
+    public void loop() {
+        pid.setCoefficients(
+                PIDTunerConfig.kP,
+                PIDTunerConfig.kI,
+                PIDTunerConfig.kD
+        );
+        int currentTicks = motor.getCurrentPosition();
+        double power = pid.update(
+                PIDTunerConfig.targetTicks,
+                currentTicks
+        );
+        motor.setPower(power);
+        telemetryM.addData("Position", currentTicks);
+        telemetryM.addData("Power", power);
+        telemetryM.addData("Target", PIDTunerConfig.targetTicks);
+        telemetryM.update(telemetry);
+    }
+}
